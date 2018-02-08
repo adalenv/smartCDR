a4b4b972eca2508d584188cdaaeb53d381a0f0d4
adalenv <adalenvladi@gmail.com>
2018-02-08 17:44:17 +0100

    Design: Title Fix

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>CDR</title>
+	<title>SmartCDR</title>
 	<script type="text/javascript" src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
